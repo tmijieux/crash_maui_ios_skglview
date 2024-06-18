f40764172f40e3f98c53aa74bf2d7cf6bee0fe18
Thomas Mijieux <thomas.mijieux@aum.bio>
2024-06-18 18:25:10 +0200

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace test_skia;
+using SkiaSharp.Views.Maui.Controls;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new ContentPage {
+                    Content = new SKGLView()
+                };
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace test_skia;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
